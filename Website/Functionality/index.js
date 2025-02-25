5ac4570e6b0740e6bc6e0011a9c72891f1837bb8
Gustaw Grześkowiak <gustaw.grzeskowiak@gmail.com>
2025-02-25 20:31:20 +0100

    header changes

--- Website/Functionality/index.js
+++ Website/Functionality/index.js
@@ -2,7 +2,13 @@ function startClock() {
     const clockElement = document.getElementById('clock');
     setInterval(() => {
         const now = new Date();
-        clockElement.textContent = now.toLocaleTimeString() + ':' + now.getMilliseconds();
+        let miliseconds = now.getMilliseconds();
+        if (miliseconds < 10) {
+            miliseconds = '00' + miliseconds;
+        } else if (miliseconds < 100) {
+            miliseconds = '0' + miliseconds;
+        }
+        clockElement.textContent = now.toLocaleTimeString() + ':' + miliseconds;
     }, 1);
 }
 
